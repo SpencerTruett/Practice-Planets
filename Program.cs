4913961cd7131fb5e85d8fed5fe7982033e025a4
Spencer Truett <srtruett1993@gmail.com>
2020-02-28 10:58:37 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists {
+    class Program {
+        static void Main (string[] args) {
+            List<string> planetList = new List<string> () { "Mercury", "Mars" };
+
+            planetList.Add ("Jupiter");
+            planetList.Add ("Saturn");
+
+            List<string> lastPlanetsList = new List<string> () { "Uranus", "Neptune" };
+
+            planetList.AddRange (lastPlanetsList);
+
+            planetList.Insert (1, "Venus");
+            planetList.Insert (2, "Earth");
+
+            planetList.Add ("Pluto");
+
+            List<string> rockyPlanets = new List<string> (planetList.GetRange (0, 4));
+
+            planetList.Remove ("Pluto");
+
+            foreach (string planets in planetList) {
+                Console.WriteLine (planets);
+            }
+
+            foreach (string rocks in rockyPlanets) {
+                Console.WriteLine (rocks);
+            }
+        }
+    }
+}
